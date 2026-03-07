9e245b17c19192a67d0ba6aa68569c45b9a7cad8
TUCKO <arthuralcantara.dmf@gmail.com>
2026-03-06 22:30:33 -0300

    Create ex006.js

---
+++ ex006.js
@@ -0,0 +1,11 @@
+const temp = 24;
+
+if(temp < 10){
+    console.log("It's cold!")
+}else if(temp >= 10){
+    console.log("It's great!")
+}else if(temp >= 20){
+    console.log("Pleasant weather")
+}else {
+    console.log("Intese heat")
+}
