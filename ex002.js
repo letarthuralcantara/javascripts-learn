b3e92596ba912b866b349e58bef35a38f3da0241
TUCKO <arthuralcantara.dmf@gmail.com>
2026-03-06 22:01:10 -0300

    Create ex002.js

---
+++ ex002.js
@@ -0,0 +1,8 @@
+const a = 15;
+const b = 4;
+
+console.log("Sum:", a + b );
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
