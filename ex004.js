feb8c13f6fb45cf4d7790c931182313489ed0a77
TUCKO <arthuralcantara.dmf@gmail.com>
2026-03-06 22:20:46 -0300

    Some exercises

---
+++ ex004.js
@@ -0,0 +1,6 @@
+const productName = "Bag";
+const price = "50$";
+const quantity = "6";
+const totalStock = "9";
+
+console.log(`The product ${productName} has ${totalStock} in stock`)
---
+++ ex005.js
@@ -0,0 +1,7 @@
+const num1 = 7;
+
+if (num1 % 2 == 1 ){
+    console.log(`The numbe ${num1} is odd`)
+}else{
+    console.log(`The number ${num1} is even`)
+}
